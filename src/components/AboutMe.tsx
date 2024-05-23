f7dd039b79cd0bc8f822aaca45cb02f2852d8049
Douhwe <andy.chen.3@stonybrook.edu>
2024-05-23 12:00:13 -0400

    fixed wording

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,8 +6,12 @@ const AboutMe = () => {
             <div className='divide-space'/>
             <p className='bio-text'>
                 I'm an aspiring Software Engineer with a passion for large-scale, high-impact Services.<br/> 
-                Currently, I am pursuing my Undergraduate Degree in Computer Science at Stony Brook University. <br/>
+                I'm currently pursuing my Undergraduate Degree in Computer Science at Stony Brook University. <br/>
                 <br/>
+                I'm also a Software Development Engineer Intern at Amazon, working with <br/>
+                a team on the Amazon Connect service within AWS
+                <br/>
+                <br/>  
                 Some of my recent interests include Cloud Computing, Computer Vision, and Computer Graphics.<br/>
                 <br/>
                 Additionally, I am eager to explore new opportunities in Machine Learning and Full-Stack <br/>
@@ -17,6 +21,8 @@ const AboutMe = () => {
                 <br/>
                 Outside of Work and Academics, I'm interested in learning random Trivia. <br/>
                 I also play a lot of video games. Maybe too much. 😂
+                <br/>
+                <br/>
             </p>
         <div className='divider'></div>
         </div>
