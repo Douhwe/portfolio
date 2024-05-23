6968d2cdbcd19cf7a1c9d86623e34df1c2836073
Douhwe <andy.chen.3@stonybrook.edu>
2024-05-23 13:06:32 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Personal Portfolio
-- Hosted Through GitHub Pages
-- Update with npm run deploy
--- src/declarations.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg'; // and any other image types you use
